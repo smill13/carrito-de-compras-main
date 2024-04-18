0e106c01e897fcef8175312ff88ef7534769b7d0
Arnold Polemil <arnoldpolemil64@gmail.com>
2024-04-18 15:18:28 -0400

    Willenson Y Arnold

--- app.js
+++ app.js
@@ -195,3 +195,29 @@ function actualizarTotalCarrito(){
     document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
 
 }
+
+/*---------------------------------------------------------------*/ 
+const fondo = document.querySelector(".fondo");
+const loginlink =document.querySelector(".login-link");
+const registrarlink = document.querySelector(".registrar-link");
+const btn =document.querySelector(".btn");
+const iconocerrar = document.querySelector(".icono-cerrar");
+
+
+registrarlink.addEventListener("click", () => {
+    fondo.classList.add('active');
+});
+
+loginlink.addEventListener("click", () => {
+    fondo.classList.remove('active');
+});
+
+btn.addEventListener("click", () => {
+    fondo.classList.add('active-btn');
+});
+
+
+iconocerrar.addEventListener("click", () => {
+    fondo.classList.remove('active-btn');
+});
+/*---------------------------------------------------------------*/ 
