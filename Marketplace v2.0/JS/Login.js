df3434a015c14212441f3eceddba9f76e77e2021
Emmanuel <58371481+smill13@users.noreply.github.com>
2024-04-19 13:07:16 -0400

    Versión 2.0
    Posee registro, login, main
    Realiza las comprobaciones en base ha archivos JSON.
    Pendientes: Filtros

---
+++ Marketplace v2.0/JS/Login.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Event listener for form submission
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
+      // Get values from form inputs
+      var email = document.getElementById('floatingInput').value;
+      var password = document.getElementById('floatingPassword').value;
+
+      // Cargar los datos del registro del almacenamiento local
+      getCaracteres(function(userData) {
+          // Verificar si los datos del registro existen y coinciden con los datos ingresados
+          var found = false;
+          for (var key in userData) {
+              if (userData[key].email === email && userData[key].password === password) {
+                  found = true;
+                  break;
+              }
+          }
+          if (found) {
+              window.open("http://127.0.0.1:3000/HTML/main.html", "_blank");
+              // Aquí podrías redirigir al usuario a otra página
+          } else {
+              alert('Email o contraseña incorrectos');
+          }
+      });
+  });
+});
+
+function getCaracteres(callback) {
+  // Hacemos una solicitud HTTP para obtener el archivo JSON local
+  fetch('/Datos/usuario.json')
+      .then(response => {
+          // Verificamos si la solicitud fue exitosa
+          if (!response.ok) {
+              throw new Error('No se pudo obtener el archivo JSON');
+          }
+          // Parseamos la respuesta como JSON y llamamos a la función de callback con los datos
+          return response.json();
+      })
+      .then(data => {
+          callback(data);
+          // Luego de cargar los datos, llamamos la función para inicializar las funcionalidades del carrito
+          ready();
+      })
+      .catch(error => {
+          console.error('Error al obtener los datos:', error);
+      });
+}
---
+++ Marketplace v2.0/JS/search.js
@@ -0,0 +1,31 @@
+function search(){
+    document.getElementById("search-input")?.addEventListener("input", async ({ target: { value } }) => {
+        const items = document.getElementsByClassName("item");
+
+        await Array.from({ length: items.length }, async (_, index) => {
+            const item = items.item(index);
+
+            const valueLower = `${value}`.toLowerCase();
+            const title = `${item.children[0].textContent}`.toLowerCase();
+            const price = `${item.children[2].textContent}`.toLowerCase();
+
+            if (title.includes(valueLower) || price.includes(valueLower)) {
+                item.classList.remove("item-hide")
+            } else {
+                item.classList.add("item-hide")
+            }
+        });
+
+        const itemsHide = document.getElementsByClassName("item-hide");
+        const emptyResult = document.getElementById("empty-result");
+
+        console.log(items.length, itemsHide.length);
+
+        if (items.length === itemsHide.length) {
+            emptyResult.style.display = "flex";
+        } else {
+            emptyResult.style.display = "none";
+        }
+    });
+
+}
